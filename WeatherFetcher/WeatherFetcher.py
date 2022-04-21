800cdb1d044c138fc4a66ce13f0377d284db28a7
Rob Schuetz <schuetz.12@osu.edu>
2022-04-21 00:26:34 -0400

    Weather data updated, WeatherFetcher cleaned up.

--- WeatherFetcher/WeatherFetcher.py
+++ WeatherFetcher/WeatherFetcher.py
@@ -1,39 +1,118 @@
 import pandas as pd
 from datetime import datetime
+import time
 from meteostat import Daily, Stations
 
 
 class WeatherFetcher:
-    def __init__(self, data, start_date):
+    def __init__(self, data):
         self.input = data # pd.DataFrame with the following columns ['Longitude', 'Latitude', 'Vintage Year]
-        self.start_date = start_date # How far back to pull historical weather data
-        self.daily_weather_data = {} # Dictionary to capture returned weather data frames, shares index with self.input
+        self.daily_weather_data = {} # Dictionary to capture returned complete weather data frames, indexed by {Vintage Year}-{Region}
+        self.cleaned_weather_data = {} # Dictionary to capture imputed data
+        self.output = {} # Dictionary to capture weather for distinct year-region combinations
 
-    def get_weather(self):
+    def query_weather(self):
 
-        def get_stop_date(row):
-            """Determine when to stop pulling weather data from the year the 
-            wine was bottled"""
-            return datetime(int(row['Vintage Year']), 12, 31)
+        """
+        Query lat/long coordinates to fetch daily data over
+        necessary date ranges indicated in unique year-region combinations
+        """
 
-        def query_weather(row):
-            """Query with lat/long coordinates and get daily data"""
-
-            # Set time period
-            start = self.start_date
-            end = get_stop_date(row)
+        # Iterate through distinct year-region combinations
+        yr_df = self.input[['region', 'latitude', 'longitude']].drop_duplicates()
+        yr_mins = self.input.groupby(['region'])['vintage'].min()
+        yr_maxs = self.input.groupby(['region'])['vintage'].max()
+        yr_df = yr_df.merge(yr_mins, left_on='region', right_index=True).rename(columns = {'vintage': 'min_year'})
+        yr_df = yr_df.merge(yr_maxs, left_on='region', right_index=True).rename(columns = {'vintage': 'max_year'})
+        
+        # For each region, query data according to provided year ranges
+        for idx, row in yr_df.iterrows():
 
             # Get the closest weather station
             stations = Stations()
-            stations = stations.nearby(row['Latitude'], row['Longitude'])
+            stations = stations.nearby(row['latitude'], row['longitude'])
             station = stations.fetch(1).index.values[0]
 
             # Get daily data bewtween designated date range
+            start = datetime(int(row['min_year']), 1, 1)
+            end = datetime(int(row['max_year']), 12, 31)
             data = Daily(station, start, end)
-            return data.fetch()
+            
+            # Assign regional data to daily_weather_data dict
+            self.daily_weather_data[f'{row["region"]}'] = data.fetch()
+            
+            # Report successful weather data query
+            print(f'Data has been pulled for {row["region"]} ({row["min_year"]} - {row["max_year"]})')
+        
+        print()
+
+
+    def clean_weather_data(self):
+        """
+        Clean data according to the following guidelines:
+            1. Fill null snow and rain with monthly average / days
+            2. Nixing wind direction, wind gust, tsun columns
+            3. Convert pressure to ATM
+            4. Fill null pressure and wind speeds with monthly averages over all available years
+        """
+
+        # Go through each weather location data frame
+        for k, v in self.daily_weather_data.items():
+            
+            # Start timer
+            tic = time.perf_counter()
+
+            # Remove superfluous columns
+            df = v.drop(columns=['wdir', 'wpgt', 'tsun'])
+
+            # Fill snow NAs
+            na_values = {'snow': 0}
+            df = df.fillna(value=na_values)
+
+            # Convert pressure to ATM
+            df['pres'] = df['pres'] * 0.0009869233 # originally recorded in hPa
+
+            # Fill columns with monthly averages
+            def get_monthly_average(row, column, df):
+                if pd.notna(row[column]):
+                    return row[column]
+                months = [x.month for x in df.index.to_list()]
+                monthly_data = df.loc[[m == row.name.month for m in months]][column]
+                monthly_mean = monthly_data.dropna().mean()
+                return monthly_mean
+
+            # For float columns
+            float_columns = [d_k for d_k, d_v in df.dtypes.to_dict().items() if d_v == 'float']
+            for c in float_columns:
+                df[c] = df.apply(get_monthly_average, axis = 1, column = c, df = df)
+
+            # Restrict dates to those occuring during growing season of the provided vintage year
+            self.cleaned_weather_data[k] = df.reset_index()
+
+            # Report successful weather data query
+            toc = time.perf_counter()
+            print(f'Data has been cleaned for {k} in {toc - tic:0.4f} seconds')
+
+        print()
+
+        
+    def filter_weather_data(self):
+
+        """
+        Once necessary data has been retrieved from the meteostat Daily object 
+        and has been properly cleaned, we need to break it into necessary year_region data frames
+        """
 
         # Run the functions on every row in the provided database
-        # self.data['weather_data'] = self.data.apply(query_weather, axis = 1)
-        for idx, row in self.input.iterrows():
-            self.daily_weather_data[idx] = query_weather(row)
+        for idx, row in self.input[['region', 'vintage']].drop_duplicates().iterrows():
 
+            # Read in data for the designated region
+            region_data = self.cleaned_weather_data[f'{row["region"]}']
+            self.output[f'{row["region"]}-{row["vintage"]}'] = region_data.loc[
+                (region_data['time'] >= datetime(int(row["vintage"]), 3, 1))
+            & 
+                (region_data['time'] <= datetime(int(row["vintage"]), 10, 31))
+            ].reset_index(drop=True)
+
+            # Report successful filtering 
+            print(f'Data filtered for {row["region"]} ({row["vintage"]})')
