ca5d14cc1bd89f844ddb7786a5926d51238e831b
kingston487 <patrckjms@gmail.com>
2022-04-25 19:32:52 -0400

    Finished project

--- scripts/CNN.py
+++ scripts/CNN.py
@@ -6,14 +6,15 @@ and convert them into rolling months (~180, n filters,)
 from tensorflow.keras.layers import Layer, Conv1D, AveragePooling1D, MaxPooling1D
 
 class CNNBlock(Layer):
-    def __init__(self, wfilt=32, mfilt=32, pool='avg', act='relu'):
+    def __init__(self, wfilt=32, mfilt=32, pool='max', act='relu'):
         super(CNNBlock, self).__init__()
-        self.conv_week = Conv1D(filters=wfilt, kernel_size=7, activation=act, name='WeekConv')
-        self.conv_month = Conv1D(filters=mfilt, kernel_size=30, activation=act, name='MonthConv')
+        self.conv_week = Conv1D(filters=wfilt, kernel_size=7, activation=act, bias_initializer='glorot_uniform', name='WeekConv')
+        self.conv_month = Conv1D(filters=mfilt, kernel_size=4, activation=act, bias_initializer='glorot_uniform', name='MonthConv')
+        self.pool = pool
         self.apool_month = AveragePooling1D(pool_size=1, name='AvgMonthPool')
         self.mpool_month = MaxPooling1D(pool_size=1, name='MaxMonthPool')
 
-    def call(self, inputs):
+    def __call__(self, inputs):
         x = self.conv_week(inputs)
         x = self.conv_month(x)
         if self.pool == 'avg':
